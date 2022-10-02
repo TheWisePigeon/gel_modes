d8e8d771c13a795da7b38bcc56dbdd53e83b23dd
TheWisePigeon <zozozozeph@gmail.com>
2022-10-02 11:44:08 +0000

    feat: added json response

--- api/handler.go
+++ api/handler.go
@@ -1,10 +1,25 @@
 package api
 
 import (
+	// "encoding/json"
 	"fmt"
 	"net/http"
+	// "encoding/json"
 )
+
+type JsonResponse struct {
+    Message string
+    HttpCode int
+    Failed bool
+}
+
 
 func Handler(w http.ResponseWriter, req *http.Request){
 	fmt.Fprintf(w, "<h1>Hi from Go :)</h1>")
+	// u, err := json.Marshal(JsonResponse{Message: "Hello gopher :)", HttpCode: 200, Failed: false})
+    //     if err != nil {
+    //         panic(err)
+    //     }
+	// w.Header().Set("Content-Type", "application/json")
+	// json.NewEncoder(w).Encode(u) 
 }
